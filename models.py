64a6d9c5f76bede1b085bce118b7f7d51ef7e162
Naman Tyagi <namantyagi@Namans-MacBook-Pro.local>
2020-12-20 22:58:47 +0530

    Neural Net function added

--- models.py
+++ models.py
@@ -135,6 +135,33 @@ def apply_RF(X_train, Y_train, X_test, Y_test):
     Y_pred = clf.predict(X_test)
     print(confusion_matrix(Y_test, Y_pred))
 
+"""
+    Applying Neural Network with cv = 4.
+    Doing Hyperparameter Tuning using Grid Search Technique.
+    Output -
+        1. Will generate a table of all train and validation score corresponding to each set of hyperparameters.
+        2. Give Test Accuracy using the best model.
+        3. Generate Confusion Matrix.
+        4. Generate Relevant Graphs. 
+"""
+
+def apply_Neural_Net(X, Y):
+    params = {'random_state' : [0], 'hidden_layer_sizes':[(264, 64, 32)], 'max_iter':[500], 'alpha':[0.001]}
+    params['activation'] = ['tanh','relu','logistic','identity']
+    clf = GridSearchCV(MLPClassifier(), param_grid = params, return_train_score = True, verbose = 5, n_jobs = -1, cv = 4)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0, stratify = Y)
+    clf.fit(X_train,Y_train)
+    results = pd.DataFrame(clf.cv_results_)
+    results = results[['params','mean_train_score','mean_test_score']]
+    print (results.to_string())
+    x = ['tanh', 'relu','logistic','identity']
+    y1 = list(results['mean_train_score'])
+    y2 = list(results['mean_test_score'])
+    graph_plot(x, y1, y2, 'Activation Function', 'Accuracy vs Activation Function', 'img/act_func.png')
+    ypred = clf.predict(X_test)
+    print ("Accuracy on Testing Data is ",accuracy_score(ypred, Y_test))
+    print (confusion_matrix(Y_test, ypred))
+    return x, y1, y2
 
 
 
